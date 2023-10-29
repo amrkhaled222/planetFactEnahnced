cbf2b7f3c386cea395cbd2f3a73e4affb93e18d3
Amr Khaled <113113874+amrkhaled222@users.noreply.github.com>
2023-10-29 14:36:50 +0200

    Update Planet.jsx

--- src/component/planet/Planet.jsx
+++ src/component/planet/Planet.jsx
@@ -119,7 +119,7 @@ export default function Planet(data) {
 					}}
 					onClick={handlebutton}
 					value="structure">
-					{!isitmobile && ` ${(<span>02</span>)} internal`}  structure
+{!isitmobile && <span>02</span>} {!isitmobile && "internal"}structure
 				</button>
 				<button
 					style={{
@@ -133,7 +133,7 @@ export default function Planet(data) {
 					}}
 					onClick={handlebutton}
 					value="geology">
-					{!isitmobile && `${(<span>03</span>)} surface`} geology
+					{!isitmobile && <span>03</span>} {!isitmobile && "surface"} geology
 				</button>
 			</div>
 		);
